33156cd90cc88213c29c9c7edb0e2d474c8e5206
zsofieppel <eppel.zsofi@gmail.com>
2023-06-20 16:16:36 +0200

    asset mappa, elso valtozok

--- minesweeper.js
+++ minesweeper.js
@@ -1,1 +1,10 @@
-console.log("Hello, világ!");
+const image = document.getElementById('hidden');
+const canvas = document.getElementById('myCanvas');
+const c = canvas.getContext('2d');
+
+const size = 50;
+let x = 0;
+let y = 0;
+
+c.drawImage(image, x, y, size, size); // A kép bal felső sarka (0,0) koordinátára kerül
+c.drawImage(image, x + size, y + size, size, size);
