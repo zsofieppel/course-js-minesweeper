1316c682efbe9b81b74a8fe2eef9afec271fe593
zsofieppel <eppel.zsofi@gmail.com>
2023-06-20 16:48:27 +0200

    drawimage fuggveny hozzaadasa

--- minesweeper.js
+++ minesweeper.js
@@ -3,8 +3,10 @@ const canvas = document.getElementById('myCanvas');
 const c = canvas.getContext('2d');
 
 const size = 50;
-let x = 0;
-let y = 0;
 
-c.drawImage(image, x, y, size, size); // A kép bal felső sarka (0,0) koordinátára kerül
-c.drawImage(image, x + size, y + size, size, size);
+drawImage(0, 0);
+drawImage(100, 100);
+
+function drawImage(x, y) {
+    c.drawImage(image, x, y, size, size);
+}
