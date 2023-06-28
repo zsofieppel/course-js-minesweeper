123c0674484c289a52ed2aa94e8c47734802fef7
zsofieppel <eppel.zsofi@gmail.com>
2023-06-28 15:33:59 +0200

    random aknak

--- minesweeper.js
+++ minesweeper.js
@@ -5,13 +5,42 @@ const c = canvas.getContext('2d');
 const size = 50;
 const columns = canvas.width / size;
 const rows = canvas.height / size;
+const mine = 'mine';
+const mineCount = 20;
 
-let map = [
-    [9, 0, 0, 1, 1, 1, 0, 0], 
-    [0, 0, 0, 1, 9, 1, 0, 0]
-];
+let map = createMap();
+placeMines(map, mineCount);
+
+
+console.log(map);
 
 drawMap();
+
+function placeMines(map, mineCount) {
+    let mines = 0;
+    while (mines < mineCount) {
+        let x = Math.floor(Math.random() * columns);
+        let y = Math.floor(Math.random() * rows);
+        if (map[y][x] !== mine) {
+            map[y][x] = mine;
+            mines++;
+        }
+    }
+}
+
+
+
+function createMap() {
+    let map = [];
+    for (let j = 0; j < rows; j++) {
+        let row = [];
+        for (let i = 0; i < columns; i++) {
+            row[i] = 0;
+        }
+        map[j] = row;
+    }
+    return map;
+}
 
 function drawMap() {
     for (let i = 0; i < columns; i++) {
